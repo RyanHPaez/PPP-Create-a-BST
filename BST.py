5194af33d514eed0e182e2350a2e03980e0fc0b4
RyanHPaez <solosnail@gmail.com>
2022-07-16 13:14:58 -0700

    changed font size on navbar

--- BST.py
+++ BST.py
@@ -1,4 +1,7 @@
 #Part 1: Create a BSTNode class
-
+class Node:
+	def __init__(self,data=None):
+    	self.data = data
+		self.next = None
 #Part 2: Create a BST class
   #Part 3: Add functionality to your BST class
